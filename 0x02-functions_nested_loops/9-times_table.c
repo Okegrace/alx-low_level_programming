c1c8cbe302cbb5e674ade434b210d87e6ce14903
Okegrace <oketogunboluwatife@gmail.com>
2022-09-13 21:04:39 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,12 +14,12 @@ void times_table(void)
 		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
-			_putchar('void');
+			_putchar(' ');
 
 			prod = num * mult;
 
 			if (prod <= 9)
-				-putchar('void');
+				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
 			_putchar((prod % 10) + '0');
