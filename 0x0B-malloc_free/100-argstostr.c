e28aaa76eb5db5d9ee9939f19d30c8329e6f25d0
Okegrace <oketogunboluwatife@gmail.com>
2022-10-03 12:28:12 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -18,7 +18,7 @@ int len(char *str)
 }
 
 /**
- * argstostr - a function that concatenates all the arguments of str
+ * argstostr - a function that concatenates all the arguments of the program
  * @ac: count of args passed to the function
  * @av: array of arguments
  *
