c338fc2aff22df4ff10929286b30e0b48e7f4796
Okegrace <oketogunboluwatife@gmail.com>
2022-10-03 12:26:13 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -7,7 +7,7 @@
  */
 int len(char *str)
 {
-	int len = 0
+	int len = 0;
 
 		if (str != NULL)
 		{
@@ -18,7 +18,7 @@ int len(char *str)
 }
 
 /**
- * argstostr - a function that concatenates all the arguments in str
+ * argstostr - a function that concatenates all the arguments of str
  * @ac: count of args passed to the function
  * @av: array of arguments
  *
@@ -34,24 +34,3 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 
 	while (ac--)
-		sum += (len(av[ac]) + 1);
-	new_string = (char *) malloc(sum + 1);
-
-	if (new_string != NULL)
-	{
-		while (k < i)
-		{
-			for (j = 0; av[k][j] != '\0'; j++)
-				new_string[j + temp] = av[k][j];
-			new_string[temp + j] = '\n';
-			temp += (j + 1);
-			k++;
-		}
-		new_string[temp] = '\0';
-	}
-	else
-	{
-		return (NULL);
-	}
-	return (new_string);
-}
